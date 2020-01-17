84c909469575d2d6a957ddadc0a1951e34c6dc09
kamiyama0304 <59788104+kamiyama0304@users.noreply.github.com>
2020-01-17 17:14:15 +0900

    create.group

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :users, only: [:edit,:update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
