1275dbe7ff9578ac4c496f84273659a3c861d7e6
kamiyama0304 <59788104+kamiyama0304@users.noreply.github.com>
2020-01-17 16:29:09 +0900

    create.group2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'groups#index'
   resources :messages, only:[:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
+  
   resources :users, only: [:edit,:update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
